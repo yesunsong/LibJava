e8a9f345cc6458c91aa63a8a2a08d3a150844a9b
yesunsong <1042282500@qq.com>
2016-11-27 20:45:09 +0800

    FileUtils增加ReadFile() 增加StringUtils

--- src/cn/xm/yss/Main.java
+++ src/cn/xm/yss/Main.java
@@ -1,11 +1,13 @@
 package cn.xm.yss;
 
+import test.FileUtilsTest;
 import test.StringUtilsTest;
 
 public class Main {
 
 	public static void main(String[] args) {
-		(new StringUtilsTest()).test();		
+		(new StringUtilsTest()).test();	
+		(new FileUtilsTest()).test();
 	}
 	
 }
--- src/test/StringUtilsTest.java
+++ src/test/StringUtilsTest.java
@@ -1,11 +1,20 @@
 package test;
 
+import java.util.ArrayList;
+
+import cn.xm.yss.JavaMacro;
+import cn.xm.yss.StringUtils;
 import test.base.BaseTest;
 
 public class StringUtilsTest extends BaseTest {
 
 	@Override
 	public void test(){
-		System.out.println("test StringUtilsTest");
+		ArrayList<String> ExcludedDirs =new ArrayList<String>();
+		ExcludedDirs.add("G:/Myself/tools_project");		
+		StringUtils.getInstance().replace(ExcludedDirs,JavaMacro.SLASH, JavaMacro.BACKSLASH);
+		System.out.println(ExcludedDirs);
+		//
+		
 	}
 }
